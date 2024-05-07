7d4e79055c85a7ea0b74304e1dd5e9dd76e5ca44
zshakirova <zshakirova@ucsd.edu>
2024-05-06 20:28:47 -0700

    changing

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,7 +2,8 @@
 
 test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
-});
-test('adds 1 + 2 to equal 3', () => {
+ });
+ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1,2)).toBe(3);
-});
+ });
+ 
